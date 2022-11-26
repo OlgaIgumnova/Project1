cf370cbdb85aefccbc558da61c1ad359e7c16cb4
Olga <o2igumn@gmail.ru>
2022-11-26 19:14:14 +0900

    add Задача 10

---
+++ Zadanie10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5; 782 -> 8; 918 -> 1
+Console.WriteLine();
+
+string str1 = 456.ToString();
+Console.WriteLine(str1[1]);
+
+ string str2 = 782.ToString();
+Console.WriteLine(str2[1]);
+
+string str3 = 918.ToString();
+Console.WriteLine(str3[1]);
